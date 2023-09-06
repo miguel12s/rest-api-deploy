52f823e7143e65c1e370498ee7cbb0deecb72ac5
miguel12s <87570624+miguel12s@users.noreply.github.com>
2023-09-06 10:24:00 -0500

    commit

---
+++ app.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const movies = require('./movies.json')
+const app = express()
+const crypto = require('node:crypto')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+const cors = require('cors')
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hola mundo' })
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    res.status(404).json({ message: 'Movie not found' })
+  }
+  movies.splice(movieIndex, 1)
+  res.json({ message: 'The movie is delete' })
+})
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  console.log(id)
+  const movie = movies.find(movie =>
+    movie.id === id
+  )
+  console.log(movie)
+  if (movie) {
+    return res.json(movie)
+  }
+  res.status(404).json({ message: 'movie not found' })
+})
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) { res.status(404).json({ message: 'Movie not found' }) }
+
+  console.log(movies[movieIndex])
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+  res.json(updateMovie)
+}
+
+)
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+//   }
+//   res.send()
+// })
+app.listen(4000, (req, res) => {
+  console.log('servidor escuchando en el puerto 4000')
+})
